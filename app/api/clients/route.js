531b4e488b25d657f77269c0808d884de45f82b7
Muneeb <muneeburrehman440@gmail.com>
2023-12-09 23:05:33 +0500

    added resend email functionality

--- app/api/clients/route.js
+++ app/api/clients/route.js
@@ -2,7 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 const { v4: uuidv4 } = require("uuid");
 const prisma = new PrismaClient();
-
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(request) {
   try {
     const client = await request.json();
@@ -26,6 +27,8 @@ export async function POST(request) {
         },
       },
     });
+
+    sendResendEmail(userId, clientData.name, clientData.email, false);
 
     return NextResponse.json({
       newClient,
@@ -114,6 +117,8 @@ export async function PUT(request) {
       },
     });
 
+    sendResendEmail(userId, clientData.name, clientData.email, true);
+
     return Response.json({
       updatedClient,
       message: "Client updated successfully",
@@ -127,3 +132,59 @@ export async function PUT(request) {
     );
   }
 }
+
+const sendResendEmail = async (userId, name, email, updated) => {
+  try {
+    const data = await resend.emails.send({
+      from: "Client Type 1 <onboarding@resend.dev>",
+      to: ["swiftservices14@gmail.com"],
+      subject: `${updated ? "Updated" : "New"} Response from ${name}}`,
+      html: `<html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 20px;
+          }
+          .container {
+            max-width: 600px;
+            margin: auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+          }
+          h1 {
+            text-align: center;
+          }
+          p {
+            margin-bottom: 20px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>${updated ? "Updated" : "New"} Response from Client Type 1</h1>
+          <p>Dear Team,</p>
+          <p>${
+            updated ? "An updated" : "A new"
+          } response has been received from the SwiftServices form</p> <br>
+          <p><strong>Name: </strong> ${name} <br> <strong>Email: </strong> ${email} <br> <strong>User ID: </strong> ${userId}</p>
+          <!-- Add more details about the response -->
+          <p>You can log in to the admin panel or visit the form dashboard to view the complete response details.</p>
+          <p>Thank you!</p>
+          <p>Best Regards,<br>SwiftServices Team</p>
+        </div>
+      </body>
+      </html>`,
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+};
